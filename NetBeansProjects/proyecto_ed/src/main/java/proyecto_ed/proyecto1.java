fed7eb7075b6eaec5283149a2824711fc7ef6d7e
Ruben <rubenrodriguezmoreno5@hotmail.com>
2023-10-26 09:43:56 +0200

    Este codigo es un java en el que pones un base y un exponente y te da el calculo de la potencia.

---
+++ NetBeansProjects/proyecto_ed/src/main/java/proyecto_ed/proyecto1.java
@@ -0,0 +1,30 @@
+
+package proyecto_ed;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rubén Rodríguez Moreno
+ */
+public class proyecto1 {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int base;
+        int exponente;
+        int resultado=1;
+        System.out.println("Introduce una base: ");
+         base = sc.nextInt();
+        System.out.println("Introduce un exponente: ");
+         exponente = sc.nextInt();
+            if(exponente<0){
+            base=1/base;            
+                           }
+        for(int contador=1;contador<=exponente;contador++){
+        resultado = resultado*base;
+                                                          } 
+       System.out.println("El resultado de la potencia "+resultado); 
+    }
+    
+}
